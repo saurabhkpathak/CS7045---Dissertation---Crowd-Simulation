cca02c190336ce5a3eb4e6dde7a1bc0e55922a36
Saurabh Pathak <pathaks@tcd.ie>
2017-05-26 13:53:44 +0100

    adding sensors and movement to the agent

---
+++ Crowd Simulation/Assets/Scripts/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour {
+
+	public float sensorLength = 5.0f;
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.DrawRay (transform.position, transform.forward * (sensorLength + transform.localScale.z));
+		Gizmos.DrawRay (transform.position, -transform.forward * (sensorLength + transform.localScale.z));
+		Gizmos.DrawRay (transform.position, transform.right * (sensorLength + transform.localScale.x));
+		Gizmos.DrawRay (transform.position, -transform.right * (sensorLength + transform.localScale.x));
+	}
+}
