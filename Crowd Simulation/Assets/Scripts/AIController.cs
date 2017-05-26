e846ef07117143c553dfa2977d6b79671cb1c4b8
Saurabh Pathak <pathaks@tcd.ie>
2017-05-26 14:20:18 +0100

    finishing the basic sensor detection and changing direction functionality.

--- Crowd Simulation/Assets/Scripts/AIController.cs
+++ Crowd Simulation/Assets/Scripts/AIController.cs
@@ -6,15 +6,68 @@ public class AIController : MonoBehaviour {
 
 	public float sensorLength = 5.0f;
 	public float speed = 10.0f;
+	float directionValue = 1.0f;
+	float turnValue = 0.0f;
+	public float turnSpeed = 50.0f;
+	Collider myCollider;
 
 	// Use this for initialization
 	void Start () {
-		
+		myCollider = transform.GetComponent<Collider> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += transform.forward * speed * Time.deltaTime;
+		RaycastHit hit;
+		int flag = 0;
+
+		// Right Sensor
+		if (Physics.Raycast (transform.position, transform.right, out hit, (sensorLength + transform.localScale.x))) {
+			if (hit.collider.tag != "Obstacle" || hit.collider == myCollider) {
+				return;
+			}
+			turnValue -= 1;
+			flag++;
+		}
+
+		// Left Sensor
+		if (Physics.Raycast (transform.position, -transform.right, out hit, (sensorLength + transform.localScale.x))) {
+			if (hit.collider.tag != "Obstacle" || hit.collider == myCollider) {
+				return;
+			}
+			turnValue += 1;
+			flag++;
+		}
+
+		// Front Sensor
+		if (Physics.Raycast (transform.position, transform.forward, out hit, (sensorLength + transform.localScale.z))) {
+			if (hit.collider.tag != "Obstacle" || hit.collider == myCollider) {
+				return;
+			}
+			if (directionValue == 1.0f) {
+				directionValue = -1;
+			}
+			flag++;
+		}
+
+		// Back Sensor
+		if (Physics.Raycast (transform.position, -transform.forward, out hit, (sensorLength + transform.localScale.z))) {
+			if (hit.collider.tag != "Obstacle" || hit.collider == myCollider) {
+				return;
+			}
+			if (directionValue == -1.0f) {
+				directionValue = 1;
+			}
+			flag++;
+		}
+
+		if (flag == 0) {
+			turnValue = 0;
+		}
+
+		transform.Rotate (Vector3.up * (turnSpeed * turnValue) * Time.deltaTime);
+
+		transform.position += transform.forward * (speed * directionValue) * Time.deltaTime;
 	}
 
 	void OnDrawGizmos () {
