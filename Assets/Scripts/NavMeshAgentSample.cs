d3dda5a8a7d780f0f158655b7d8eb70c2f636a7a
Saurabh Pathak <pathaks@tcd.ie>
2017-08-07 16:46:19 +0100

    Adding exits to the scene

--- Assets/Scripts/NavMeshAgentSample.cs
+++ Assets/Scripts/NavMeshAgentSample.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public class NavMeshAgentSample : MonoBehaviour {
 
@@ -7,30 +8,53 @@ public class NavMeshAgentSample : MonoBehaviour {
     NavMeshAgent agent;
 	System.Random r;
     float speedCache;
-	// Use this for initialization
-	void Start () {
+    ArrayList exits;
+    // Use this for initialization
+    void Start () {
 		r = new System.Random ();
         agent = GetComponent<NavMeshAgent>();
 		int rDest = r.Next (1, 11);
 		agent.transform.position = GameObject.Find ("Destination " + rDest).GetComponent<Transform> ().position;
         speedCache = agent.speed;
+        exits = new ArrayList();
+        populateExits(exits);
     }
 	
 	// Update is called once per frame
 	void Update () {
 		if(Vector3.Distance(agent.transform.position, target.transform.position) < 1.5) {
-			Debug.Log("objects touching");
 			int rDest = r.Next (1, 11);
 			target = GameObject.Find ("Destination " + rDest).GetComponent<Transform> ();
 		}
         agent.SetDestination(target.position);
-        if (Input.GetKeyDown("s"))
-        {
+        if (Input.GetKeyDown("s")) {
             agent.speed = 0;
         }
-        if (Input.GetKeyDown("m"))
-        {
+        if (Input.GetKeyDown("m")) {
             agent.speed = speedCache;
         }
+        if (Input.GetKeyDown("e")) {
+            agent.speed = 0;
+            //float nearestExit = Vector3.Distance(agent.transform.position);
+        }
+    }
+
+    void populateExits(ArrayList exits)
+    {
+        int i = 1;
+		GameObject exit = GameObject.Find ("Exit " + i);
+		while (exit != null) {
+			exits.Add (exit);
+			i++;
+			exit = GameObject.Find ("Exit " + i);
+        }
+    }
+
+    float getNearestExit()
+    {
+        float minDistance = Mathf.Infinity;
+		for (var i = 1; i <= exits.Count; i++) {
+		}
+        return minDistance;
     }
 }
