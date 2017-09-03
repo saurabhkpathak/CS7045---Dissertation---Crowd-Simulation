90635bac4516081fec4f3c48b12667240d9855c1
Saurabh Pathak <pathaks@tcd.ie>
2017-09-03 16:39:46 +0100

    finishing up with simulation of all 3 scenes with 1000 agents

---
+++ Assets/ChildCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class ChildCounter : MonoBehaviour {
+    Stopwatch timer;
+    // Use this for initialization
+    void Start () {
+        timer = new Stopwatch();
+        UnityEngine.Debug.Log(transform.childCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("e"))
+        {
+            timer.Start();
+        }
+        UnityEngine.Debug.Log(timer.ElapsedMilliseconds);
+    }
+}
